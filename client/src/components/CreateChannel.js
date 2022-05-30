850c602fa93ee0733fe23887a8d887502d735386
yodim <mmbatrone@gmail.com>
2022-05-31 00:13:30 +0100

    create create channel page

--- client/src/components/CreateChannel.js
+++ client/src/components/CreateChannel.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Grid, Button, CssBaseline, Divider, Typography,  } from '@mui/material/'
+import InputField from './../layout/InputField'
 
 function CreateChannel() {
   return (
-    <div>CreateChannel</div>
+    <Grid container direction="column" spacing={1} sx={styles.container}>
+        <Grid item >
+            <InputField id="outlined-basic" label="Channel name" variant="outlined" />
+        </Grid>
+        <Grid item >
+            <InputField id="outlined-basic" label="Add tags" variant="outlined" />
+        </Grid>
+    </Grid>
   )
+}
+
+const styles = {
+    container: {
+        marginLeft: '10px',
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    button: {
+        alignItems:'right' //??
+    }
 }
 
 export default CreateChannel
