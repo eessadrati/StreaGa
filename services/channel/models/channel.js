7e00f07d08ffbc5035145a3f76fed3e1a71007eb
Hachami <mohamedhachami68@gmail.com>
2022-06-07 21:04:46 +0100

    channel service

---
+++ services/channel/models/channel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const channelSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  _id: mongoose.Schema.Types.ObjectId,
+  userId: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    logo_url: {
+      type: String,
+      default: "",
+    },
+    logo_cloudinary_id: {
+      type: String,
+      default: "",
+    },
+  },
+  cover: {
+    cover_url: {
+      type: String,
+      default: "",
+    },
+    cover_cloudinary_id: {
+      type: String,
+      default: "",
+    },
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  subscribers: {
+    type: Array,
+    default: [],
+  },
+  tags: {
+    type: Array,
+    default: [],
+  },
+  videos: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("channel", channelSchema);
