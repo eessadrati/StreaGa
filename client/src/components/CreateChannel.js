23fe4e08d3ccc71bfcadbc7b62ce4397b6ab212b
yodim <mmbatrone@gmail.com>
2022-06-08 23:29:17 +0100

    createChannel

--- client/src/components/CreateChannel.js
+++ client/src/components/CreateChannel.js
@@ -5,6 +5,8 @@ import TagsInput from './../layout/TagsInput'
 
 function CreateChannel() {
   const [tagsList,setTagsList]=useState([]);
+  const [channelName, setChannelName] = useState("");
+  const [channelTags, setChannelTags] = useState([]);
 
   const handleTagsList =(items) =>{
     console.log(items)
@@ -14,18 +16,20 @@ function CreateChannel() {
   return (
     <Grid container direction="row" spacing={1} sx={styles.container}>
         <Grid item xs={12}>          
-            <InputField  id="outlined-basic" label="Channel name" variant="outlined" sx={{width:'400px'}}/>
+            <InputField  id="outlined-basic" label="Channel name" variant="outlined" sx={{width:'400px'}}
+              value={channelName} onChange={e => setChannelName(e.target.value)} />
         </Grid>
         <Grid item sx={{marginTop:'2vh'}}>
             <TagsInput 
-            sx={{width:'400px'}}
-            selectedTags={handleTagsList}
-            variant="outlined"
-            id="tags"
-            name="tags"
-            tags={tagsList}
-            placeholder="add tag"
-            id="outlined-basic" label="Tags" variant="outlined" 
+              sx={{width:'400px'}}
+              selectedTags={handleTagsList}
+              variant="outlined"
+              id="tags"
+              name="tags"
+              tags={tagsList}
+              placeholder="add tag"
+              id="outlined-basic" label="Tags" variant="outlined" 
+              value={channelTags} onChange={e => setChannelTags(e.target.value)}
             />
         </Grid>
     </Grid>
